7ddcf7e8555aa14b1789022198fe7d13998c2c0b
Razmik Tovmasyan <razmikt@internal.synopsys.com>
2018-08-16 16:13:43 +0400

    Add definations in the Node class

--- Node.cpp
+++ Node.cpp
@@ -4,3 +4,12 @@
 
 Node::Node(int v) : value(v), left(nullptr), right(nullptr), parent(nullptr) { }
 Node::~Node() { }
+
+int Node::GetValue() { return value; }
+void Node::SetValue(int v) { value = v; }
+Node * Node::GetLeft() { return left; }
+void Node::SetLeft(Node * l) { left = l; }
+Node * Node::GetRight() { return right; }
+void Node::SetRight(Node * r) { right = r; }
+Node * Node::GetParent() { return parent; }
+void Node::SetParent(Node * p) { parent = p; }
