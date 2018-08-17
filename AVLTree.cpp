e19ebed41d399c8bbe3cb1358d9b40da7b94d351
Razmik Tovmasyan <razmikt@internal.synopsys.com>
2018-08-17 12:25:15 +0400

    Add implementation of the BalanceHelper function

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -24,7 +24,52 @@ Node * AVLTree::Remove(int value)
 
 Node * AVLTree::BalanceHelper(Node * node)
 {
-	return node;
+	if (node == nullptr) return nullptr;
+
+	AVLNode * avlNode = dynamic_cast<AVLNode *> (node);
+
+	// Update height of this node
+	avlNode->SetHeight(1 + Max(Height(avlNode->GetLeft()), Height(avlNode->GetRight())));
+
+	// Get the balance of this node
+	int balance = Balance(avlNode);
+
+	if (balance > 1)
+	{
+		int childBalance = Balance(dynamic_cast<AVLNode *> (node->GetLeft()));
+		if (childBalance >= 0)
+		// Left - Left Case
+		{
+			RightRotation(node->GetLeft());
+		}
+		else
+		// Left - Right Case
+		{
+			LeftRotation(node->GetLeft()->GetRight());
+			RightRotation(node->GetLeft());
+		}
+
+		dynamic_cast<AVLNode *> (node)->SetHeight(1 + Max(Height(node->GetLeft()), Height(node->GetRight())));
+	}
+	else if (balance < -1)
+	{
+		int childBalance = Balance(dynamic_cast<AVLNode *> (node->GetRight()));
+		if (childBalance <= 0)
+		// Right - Right Case
+		{
+			LeftRotation(node->GetRight());
+		}
+		else
+		// Right - Left Case
+		{
+			RightRotation(node->GetRight()->GetLeft());
+			LeftRotation(node->GetRight());
+		}
+
+		dynamic_cast<AVLNode *> (node)->SetHeight(1 + Max(Height(node->GetLeft()), Height(node->GetRight())));
+	}
+
+	return BalanceHelper(node->GetParent());
 }
 
 int AVLTree::Height(Node * node) { return (node == nullptr) ? 0 : dynamic_cast<AVLNode *> (node)->GetHeight(); }
