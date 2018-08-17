bb9852b64b4e5d590aac926d4ff9972f620a8a05
Razmik Tovmasyan <razmikt@internal.synopsys.com>
2018-08-17 11:52:52 +0400

    Add helper functions

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -3,3 +3,7 @@
 
 AVLTree::AVLTree() { }
 AVLTree::~AVLTree() { }
+
+int AVLTree::Height(Node * node) { return (node == nullptr) ? 0 : dynamic_cast<AVLNode *> (node)->GetHeight(); }
+int AVLTree::Max(int a, int b) { return (a > b) ? a : b; }
+int AVLTree::Balance(Node * node) { return (node == nullptr) ? 0 : Height(node->GetLeft()) - Height(node->GetRight()); }
