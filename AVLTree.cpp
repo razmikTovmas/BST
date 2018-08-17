afa0dbee8e6adc295cf7c44cf13222bc11817a15
Razmik Tovmasyan <razmikt@internal.synopsys.com>
2018-08-17 11:56:16 +0400

    Add Insert and Remove functions
    Balancing will be done by BalanceHelper function

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -4,6 +4,29 @@
 AVLTree::AVLTree() { }
 AVLTree::~AVLTree() { }
 
+Node * AVLTree::Insert(int value)
+{
+	Node * node = InsertHelper(root, new AVLNode(value));
+
+	BalanceHelper(node);
+
+	return node;
+}
+
+Node * AVLTree::Remove(int value)
+{
+	Node * node = RemoveHelper(FindNode(root, value));
+
+	BalanceHelper(node);
+
+	return node;
+}
+
+Node * AVLTree::BalanceHelper(Node * node)
+{
+	return node;
+}
+
 int AVLTree::Height(Node * node) { return (node == nullptr) ? 0 : dynamic_cast<AVLNode *> (node)->GetHeight(); }
 int AVLTree::Max(int a, int b) { return (a > b) ? a : b; }
 int AVLTree::Balance(Node * node) { return (node == nullptr) ? 0 : Height(node->GetLeft()) - Height(node->GetRight()); }
